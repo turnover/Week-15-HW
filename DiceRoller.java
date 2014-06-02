fb777c51efea4aef9e2c754bfa2a868785af8aa9
Luiz Gonzalez <counterparts714@gmail.com>
2014-06-01 22:55:40 -0700

    Update DiceRoller.java

--- DiceRoller.java
+++ DiceRoller.java
@@ -1,14 +1,16 @@
 public class DiceRoller implements Runnable
 {
   private final String taskName;
-  
+  //name can't be changed
   public DiceRoller(String name)
   {
     taskName = name;
+    //sets the taskName
   }
   
   public void run()
   {
+    //use of try, catch
     try
     {
       System.out.printf("%s Start \n",taskName);
@@ -23,5 +25,6 @@ public class DiceRoller implements Runnable
     }
     
     System.out.printf("%s Finished Throwing.",taskName);
+    //when the try catch is finished
   }
 }
