6092b1d1bf7188eb9e6dc0f38eab04092cee44b6
Luiz Gonzalez <counterparts714@gmail.com>
2014-06-01 22:37:04 -0700

    Create Dice.java

---
+++ Dice.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Dice
+{
+  Random random = new Random();
+  
+  public Dice()
+  {
+  
+  }
+  
+  public int roll()
+  {
+    return random.nextInt(6)+1;
+  }
+}
